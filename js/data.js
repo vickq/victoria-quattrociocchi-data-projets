e8fed63fb729788f6ae89b2de865530666eddb44
Victoria Quattrociocchi <quattrociocchivictoria@gmail.com>
2025-12-17 15:52:57 -0300

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,1 +1,163 @@
-/* data */
+const tasks = [
+    {
+        day: 1,
+        title: "",
+        preview: "",
+        difficulty: "",
+        task: "",
+        description: "",
+        suggestedChart: "",
+        tips: [],
+        bonus: ""
+    },
+    {
+        day: 2,
+        title: "",
+        preview: "",
+        difficulty: "",
+        description: ``,
+        task: ``,
+        tips: [],
+        bonus: ""
+    },
+    {
+        day: 3,
+        title: "",
+        preview: "",
+        difficulty: "",
+        task: "",
+        description: "",
+        suggestedChart: "",
+        tips: [],
+        bonus: ""
+    },
+    {
+        day: 4,
+        title: "",
+        preview: "",
+        difficulty: "",
+        description: "",
+        task: ``,
+        experts: "",
+        tips: [],
+        bonus: ""
+    },
+    {
+        day: 5,
+        title: "",
+        preview: "",
+        difficulty: "",
+        task: "",
+        description: "",
+        suggestedChart: "",
+        tips: [],
+        experts: "",
+        bonus: ""
+    },
+    {
+        day: 6,
+        title: "",
+        preview: "",
+        difficulty: "",
+        description: "",
+        task: ``,
+        tips: [],
+        bonus: ""
+    },
+    {
+        day: 7,
+        title: "",
+        preview: "",
+        difficulty: "",
+        description: "",
+        task: ``,
+        tips: [],
+        bonus: ""
+    },
+    {
+        day: 8,
+        title: "",
+        preview: "",
+        difficulty: "",
+        task: "",
+        description: "",
+        suggestedChart: "",
+        tips: [],
+        experts: "",
+        bonus: ""
+    },
+    {
+        day: 9,
+        title: "",
+        preview: "",
+        difficulty: "",
+        description: "",
+        task: ``,
+        tips: [],
+        bonus: ""
+    },
+    {
+        day: 10,
+        title: "",
+        preview: "",
+        difficulty: "",
+        task: "",
+        description: "",
+        suggestedChart: "",
+        tips: [],
+        bonus: ""
+    },
+    {
+        day: 11,
+        title: "",
+        preview: "",
+        difficulty: "",
+        task: "",
+        description: "",
+        suggestedChart: "",
+        tips: [],
+        bonus: ""
+    },
+    {
+        day: 12,
+        title: "",
+        preview: "",
+        difficulty: "",
+        task: "",
+        description: "",
+        suggestedChart: "",
+        tips: [],
+        bonus: ""
+    },
+    {
+        day: 13,
+        title: "",
+        preview: "",
+        difficulty: "",
+        description: "",
+        task: ``,
+        tips: [],
+        bonus: ""
+    },
+    {
+        day: 14,
+        title: "",
+        preview: "",
+        difficulty: "",
+        task: "",
+        description: "",
+        suggestedChart: "",
+        tips: [],
+        bonus: ""
+    },
+    {
+        day: 15,
+        title: "",
+        preview: "",
+        difficulty: "",
+        description: "",
+        task: ``,
+        tips: [],
+        bonus: ""
+    }
+];
